ed077f6a070eda9bf495b61ed7e43047fd6c8405
Ubuntu <ubuntu@ip-172-31-37-127.eu-north-1.compute.internal>
2025-09-08 08:47:03 +0000

    loops

--- for-loop2.sh
+++ for-loop2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
-for (( num=1;num<=5;num++  ))
+<<comment
+1 is for folder name
+2 is for start range
+3 is for end
+comment
+
+for (( num=$2; num<=$3; num++ ))
 do
-	rm -r demo$num
+	mkdir "$1$num"
 done
---
+++ while-loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+<<comment 
+while loop
+comment
+
+num=0
+
+while [[ $num -le 5  ]]
+do
+	echo "Lol"
+	num=$num+1
+done
